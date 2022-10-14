2a373aaa3ea57fb38da7af5b3518081397f63321
chemi <wbsldj7645@naver.com>
2022-10-14 11:21:49 +0900

    [SETTING] initial setting

---
+++ Maddori.Apple/Maddori.Apple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maddori.Apple
+//
+//  Created by Mingwan Choi on 2022/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
