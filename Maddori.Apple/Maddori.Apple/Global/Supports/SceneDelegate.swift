985985cf743bdf8fa41181f323fab2a10a3cda15
chemi <wbsldj7645@naver.com>
2022-10-17 16:56:24 +0900

    [CHORE] 코드 리뷰 반영 오타 수정 (#2)

--- Maddori.Apple/Maddori.Apple/Global/Supports/SceneDelegate.swift
+++ Maddori.Apple/Maddori.Apple/Global/Supports/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootViewControoler = UINavigationController(rootViewController: MainViewController())
-        window?.rootViewController = rootViewControoler
+        let rootViewController = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
 
