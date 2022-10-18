73108ded906b6a784ba2a3452ce06b854d3c6a67
Guel-git <eccyuna@gmail.com>
2022-10-19 01:46:31 +0900

    [ADD] CollectionView & CollectionViewCell 추가(#12)

---
+++ Maddori.Apple/Maddori.Apple/Global/UIComponent/MemberCollectionView.swift
@@ -0,0 +1,33 @@
+//
+//  MemberCollectionView.swift
+//  Maddori.Apple
+//
+//  Created by 김유나 on 2022/10/19.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MemberCollectionView: UIView {
+    
+    // MARK: - lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func render() {
+        
+    }
+    
+    func configUI() {
+        
+    }
+}
---
+++ Maddori.Apple/Maddori.Apple/Global/UIComponent/MemberCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemberCollectionViewCell.swift
+//  Maddori.Apple
+//
+//  Created by 김유나 on 2022/10/19.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MemberCollectionViewCell: BaseCollectionViewCell {
+    
+}
