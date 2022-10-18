c3fea72481ed8f9815ed9c85a777b3b4e22830cc
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-10-18 21:41:25 +0900

    [FEAT] 텍스트필드 UI구현(#11)

--- Maddori.Apple/Maddori.Apple/Screen/Signup/SignupViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Signup/SignupViewController.swift
@@ -11,12 +11,47 @@ import SnapKit
 
 final class SignupViewController: BaseViewController {
     
+    private let minNum: Int = 0
+    private let maxNum: Int = 6
+    
     // MARK: - property
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "키고에서 사용할 \n닉네임을 입력해주세요"
         label.font = .title
         label.numberOfLines = 2
+        return label
+    }()
+    
+    private let nicknameTextField: UITextField = {
+        let textField = UITextField()
+        let attributes = [
+            NSAttributedString.Key.font : UIFont.body1,
+            NSAttributedString.Key.foregroundColor : UIColor.gray500
+        ]
+        textField.backgroundColor = .white300
+        textField.attributedPlaceholder = NSAttributedString(string: "예) 진저, 호야, 성민", attributes: attributes)
+        textField.font = .body1
+        textField.layer.cornerRadius = 10
+        textField.layer.masksToBounds = true
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.white300.cgColor
+        textField.textAlignment = .left
+        textField.returnKeyType = .done
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        textField.leftView = leftPaddingView
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
+    private lazy var textLimitLabel: UILabel = {
+        let label = UILabel()
+        label.text = "\(minNum)/\(maxNum)"
+        label.font = .body2
+        label.textColor = .gray500
         return label
     }()
     
@@ -36,6 +71,19 @@ final class SignupViewController: BaseViewController {
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).inset(Size.topPadding)
             $0.leading.equalToSuperview().inset(Size.leadingTrailingPadding)
+        }
+        
+        view.addSubview(nicknameTextField)
+        nicknameTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(39)
+            $0.leading.trailing.equalToSuperview().inset(Size.leadingTrailingPadding)
+            $0.height.equalTo(56)
+        }
+        
+        view.addSubview(textLimitLabel)
+        textLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(nicknameTextField.snp.bottom).offset(4)
+            $0.trailing.equalToSuperview().inset(27)
         }
     }
     // MARK: - function
