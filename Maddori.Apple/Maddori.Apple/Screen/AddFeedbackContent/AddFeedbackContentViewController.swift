8891a1b6859beeb72faa00169df161735b436516
Guel-git <eccyuna@gmail.com>
2022-10-21 14:58:59 +0900

    [ADD] 키워드 텍스트 필드 추가 중(#19)

--- Maddori.Apple/Maddori.Apple/Screen/AddFeedbackContent/AddFeedbackContentViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/AddFeedbackContent/AddFeedbackContentViewController.swift
@@ -11,8 +11,12 @@ import SnapKit
 
 final class AddFeedbackContentViewController: BaseViewController {
     
+    private let minLength: Int = 0
+    private let maxLength: Int = 15
+    private var nickname: String = ""
+    
     // MARK: - property
-
+    
     private let backButton = BackButton()
     private let closeButton = CloseButton()
     private let addFeedbackTitleLabel: UILabel = {
@@ -30,8 +34,35 @@ final class AddFeedbackContentViewController: BaseViewController {
         return label
     }()
     private let feedbackTypeButtonView = FeedbackTypeButtonView()
+    private let feedbackKeywordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "키워드"
+        label.textColor = .black100
+        label.font = .label2
+        return label
+    }()
+    private let feedbackKeywordTextField: KigoTextField = {
+        let textField = KigoTextField()
+        textField.placeHolderText = "피드백을 한 단어로 작성해주세요"
+        return textField
+    }()
+    private lazy var textLimitLabel: UILabel = {
+        let label = UILabel()
+        label.setTextWithLineHeight(text: "\(minLength)/\(maxLength)", lineHeight: 22)
+        label.font = .body2
+        label.textColor = .gray500
+        return label
+    }()
     
     // MARK: - lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        render()
+        //setupNotificationCenter()
+        setupDelegate()
+    }
     
     override func render() {
         view.addSubview(addFeedbackTitleLabel)
@@ -52,8 +83,26 @@ final class AddFeedbackContentViewController: BaseViewController {
             $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
             $0.centerX.equalToSuperview()
         }
+        
+        view.addSubview(feedbackKeywordLabel)
+        feedbackKeywordLabel.snp.makeConstraints {
+            $0.top.equalTo(feedbackTypeButtonView.snp.bottom).offset(36)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(feedbackKeywordTextField)
+        feedbackKeywordTextField.snp.makeConstraints {
+            $0.top.equalTo(feedbackKeywordLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(textLimitLabel)
+        textLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(feedbackKeywordTextField.snp.bottom).offset(4)
+            $0.trailing.equalToSuperview().inset(27)
+        }
     }
-
+    
     // MARK: - functions
     
     override func setupNavigationBar() {
@@ -66,5 +115,71 @@ final class AddFeedbackContentViewController: BaseViewController {
         navigationItem.largeTitleDisplayMode = .never
         navigationItem.leftBarButtonItem = backButton
         navigationItem.rightBarButtonItem = closeButton
+    }
+    
+//    private func setupNotificationCenter() {
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+//    }
+    
+    private func setupDelegate() {
+        feedbackKeywordTextField.delegate = self
+    }
+    
+    override func endEditingView() {
+//        if !doneButton.isTouchInside {
+//            view.endEditing(true)
+//        }
+    }
+    
+    private func setCounter(count: Int) {
+        if count <= maxLength {
+            textLimitLabel.text = "\(count)/\(maxLength)"
+        }
+        else {
+            textLimitLabel.text = "\(maxLength)/\(maxLength)"
+        }
+    }
+    
+    private func checkMaxLength(textField: UITextField, maxLength: Int) {
+        if let text = textField.text {
+            if text.count > maxLength {
+                let endIndex = text.index(text.startIndex, offsetBy: maxLength)
+                let fixedText = text[text.startIndex..<endIndex]
+                textField.text = fixedText + " "
+                
+                DispatchQueue.main.async {
+                    self.feedbackKeywordTextField.text = String(fixedText)
+                }
+            }
+        }
+    }
+    
+    // MARK: - selector
+    
+//    @objc private func keyboardWillShow(notification:NSNotification) {
+//        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+//            UIView.animate(withDuration: 0.2, animations: {
+//                self.doneButton.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height + 10)
+//            })
+//        }
+//    }
+//
+//    @objc private func keyboardWillHide(notification:NSNotification) {
+//        UIView.animate(withDuration: 0.2, animations: {
+//            self.doneButton.transform = .identity
+//        })
+//    }
+}
+
+// MARK: - extension
+
+extension AddFeedbackContentViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        setCounter(count: textField.text?.count ?? 0)
+        checkMaxLength(textField: feedbackKeywordTextField, maxLength: maxLength)
+        
+        let hasText = feedbackKeywordTextField.hasText
+        //doneButton.isDisabled = !hasText
     }
 }
