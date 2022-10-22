13eb6b3c5480ab74cfb8b52cc2453e3829089f29
chemi <wbsldj7645@naver.com>
2022-10-22 21:50:52 +0900

    [CHORE] fatalError -> nil (#33)

--- Maddori.Apple/Maddori.Apple/Screen/AddReflection/UIComponent/ReflectionNameView.swift
+++ Maddori.Apple/Maddori.Apple/Screen/AddReflection/UIComponent/ReflectionNameView.swift
@@ -52,9 +52,7 @@ final class ReflectionNameView: UIView {
         render()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) { nil }
     
     private func render() {
         self.addSubview(reflectionNameLabel)
