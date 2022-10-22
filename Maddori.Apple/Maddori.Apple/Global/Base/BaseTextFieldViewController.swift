4930f1ec6328a480d375e7058671862cac1884e4
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-10-22 20:38:44 +0900

    [CHORE] 충돌해결

---
+++ Maddori.Apple/Maddori.Apple/Global/Base/BaseTextFieldViewController.swift
@@ -0,0 +1,154 @@
+//
+//  SignupViewController.swift
+//  Maddori.Apple
+//
+//  Created by 이성호 on 2022/10/18.
+//
+
+import UIKit
+
+import SnapKit
+
+class BaseTextFieldViewController: BaseViewController {
+    
+    private let minLength: Int = 0
+    private let maxLength: Int = 6
+    private var nickname: String = ""
+    
+    var titleText: String = ""
+    var placeholderText: String = ""
+    var buttonText: String = ""
+    
+    // MARK: - property
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.setTitleFont(text: titleText)
+        return label
+    }()
+    private lazy var nicknameTextField: KigoTextField = {
+        let textField = KigoTextField()
+        textField.placeHolderText = placeholderText
+        return textField
+    }()
+    private lazy var textLimitLabel: UILabel = {
+        let label = UILabel()
+        label.setTextWithLineHeight(text: "\(minLength)/\(maxLength)", lineHeight: 22)
+        label.font = .body2
+        label.textColor = .gray500
+        return label
+    }()
+<<<<<<< HEAD:Maddori.Apple/Maddori.Apple/Screen/Setup/BaseTextFieldViewController.swift
+    let doneButton: MainButton = {
+=======
+    private lazy var doneButton: MainButton = {
+>>>>>>> 866f47f20491192d8be9b486425bc031e0ef6f29:Maddori.Apple/Maddori.Apple/Global/Base/BaseTextFieldViewController.swift
+        let button = MainButton()
+        button.title = buttonText
+        button.isDisabled = true
+        return button
+    }()
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        render()
+        setupNotificationCenter()
+        setupDelegate()
+    }
+    
+    override func render() {
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(SizeLiteral.topPadding)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(nicknameTextField)
+        nicknameTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(40)
+            $0.centerX.equalToSuperview()
+        }
+        
+        view.addSubview(textLimitLabel)
+        textLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(nicknameTextField.snp.bottom).offset(4)
+            $0.trailing.equalToSuperview().inset(27)
+        }
+        
+        view.addSubview(doneButton)
+        doneButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(2)
+        }
+    }
+    // MARK: - function
+    
+    private func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func setupDelegate() {
+        nicknameTextField.delegate = self
+    }
+    
+    override func endEditingView() {
+        if !doneButton.isTouchInside {
+            view.endEditing(true)
+        }
+    }
+    
+    private func setCounter(count: Int) {
+        if count <= maxLength {
+            textLimitLabel.text = "\(count)/\(maxLength)"
+        }
+        else {
+            textLimitLabel.text = "\(maxLength)/\(maxLength)"
+        }
+    }
+    
+    private func checkMaxLength(textField: UITextField, maxLength: Int) {
+        if let text = textField.text {
+            if text.count > maxLength {
+                let endIndex = text.index(text.startIndex, offsetBy: maxLength)
+                let fixedText = text[text.startIndex..<endIndex]
+                textField.text = fixedText + " "
+            
+                DispatchQueue.main.async {
+                    self.nicknameTextField.text = String(fixedText)
+                }
+            }
+        }
+    }
+    
+    // MARK: - selector
+    
+    @objc private func keyboardWillShow(notification:NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.doneButton.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height + 10)
+            })
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification:NSNotification) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.doneButton.transform = .identity
+        })
+    }
+}
+
+// MARK: - Extension
+
+extension BaseTextFieldViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        setCounter(count: textField.text?.count ?? 0)
+        checkMaxLength(textField: nicknameTextField, maxLength: maxLength)
+        
+        let hasText = nicknameTextField.hasText
+        doneButton.isDisabled = !hasText
+    }
+}
