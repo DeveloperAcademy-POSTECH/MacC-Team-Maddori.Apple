10b8689d9478a44469710b3fe1384923d75df4c0
chemi <wbsldj7645@naver.com>
2022-10-23 23:19:48 +0900

    [ADD] 전달 해준사람과 타입 Label 추가 (#44)

--- Maddori.Apple/Maddori.Apple/Screen/ReflectionInfo/ReflectionInfoViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/ReflectionInfo/ReflectionInfoViewController.swift
@@ -5,4 +5,39 @@
 //  Created by Mingwan Choi on 2022/10/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class ReflectionInfoViewController: BaseViewController {
+    let viewModel: ReflectionInfoModel
+    
+    // MARK: - property
+    
+    private lazy var titleText: UILabel = {
+        let label = UILabel()
+        label.text = "\(viewModel.nickname)님이 보낸 \(viewModel.type.rawValue)"
+        label.textColor = .gray400
+        label.applyColor(to: "\(viewModel.type.rawValue)", with: .blue200)
+        label.font = .caption1
+        return label
+    }()
+    
+    // MARK: - life cycle
+    
+    init(viewModel: ReflectionInfoModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    override func render() {
+        view.addSubview(titleText)
+        titleText.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.top.equalTo(view.safeAreaLayoutGuide
+                .snp.top).inset(55)
+        }
+    }
+}
