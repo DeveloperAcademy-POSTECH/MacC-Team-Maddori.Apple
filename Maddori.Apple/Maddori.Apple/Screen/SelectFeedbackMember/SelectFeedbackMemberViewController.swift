a8771cb4d4c97122a0207d535abdcfda06b0ccfc
Guel-git <eccyuna@gmail.com>
2022-10-25 01:23:21 +0900

    [ADD] 디렉토리 추가(#48)

---
+++ Maddori.Apple/Maddori.Apple/Screen/SelectFeedbackMember/SelectFeedbackMemberViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SelectFeedbackMemberViewController.swift
+//  Maddori.Apple
+//
+//  Created by 김유나 on 2022/10/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SelectFeedbackMemberViewController: BaseViewController {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+
+}
