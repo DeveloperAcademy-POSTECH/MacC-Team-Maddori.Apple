acded1c70fb38360ba1a63a8baeb4cf6c0e06f8c
Guel-git <eccyuna@gmail.com>
2022-10-25 02:12:36 +0900

    [ADD] CollectionView & Button 추가(#48)

--- Maddori.Apple/Maddori.Apple/Screen/SelectFeedbackMember/SelectFeedbackMemberViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/SelectFeedbackMember/SelectFeedbackMemberViewController.swift
@@ -19,9 +19,16 @@ final class SelectFeedbackMemberViewController: BaseViewController {
         label.text = "피드백을 듣고 싶은\n팀원을 선택해주세요"
         label.textColor = .black100
         label.font = .title
-        label.numberOfLines = 2
+        label.numberOfLines = 0
         label.setLineSpacing()
         return label
+    }()
+    private let memberCollectionView = MemberCollectionView()
+    private lazy var feedbackDoneButton: MainButton = {
+        let button = MainButton()
+        button.title = "모든 회고 끝내기 (\(memberCollectionView.numOfSelectedMember)/\(memberCollectionView.memberList.count))"
+        button.isDisabled = true
+        return button
     }()
     
     // MARK: - life cycle
@@ -31,6 +38,19 @@ final class SelectFeedbackMemberViewController: BaseViewController {
         selectFeedbackMemberTitleLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).inset(SizeLiteral.topPadding)
             $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(memberCollectionView)
+        memberCollectionView.snp.makeConstraints {
+            $0.top.equalTo(selectFeedbackMemberTitleLabel.snp.bottom)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(feedbackDoneButton)
+        feedbackDoneButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(SizeLiteral.bottomPadding)
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
         }
     }
     
