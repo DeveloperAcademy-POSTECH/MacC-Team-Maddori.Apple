88d3c9ddb10fef2440bacc4f918f08103834f13a
Guel-git <eccyuna@gmail.com>
2022-11-02 16:33:28 +0900

    [ADD] 파일 및 디렉토리 생성(#63)

---
+++ Maddori.Apple/Maddori.Apple/Screen/MyBox/FeedbackFromMeDetail/FeedbackFromMeDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedbackFromMeDetailViewController.swift
+//  Maddori.Apple
+//
+//  Created by 김유나 on 2022/11/02.
+//
+
+import UIKit
+
+import SnapKit
+
+final class FeedbackFromMeDetailViewController: BaseViewController {
+
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+
+}
