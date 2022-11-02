59c1b6a2e47295e0da9192eb478883b5ca9f008e
Guel-git <eccyuna@gmail.com>
2022-11-02 16:43:54 +0900

    [ADD] navigation bar 생성(#63)

--- Maddori.Apple/Maddori.Apple/Screen/MyBox/FeedbackFromMeDetail/FeedbackFromMeDetailViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/MyBox/FeedbackFromMeDetail/FeedbackFromMeDetailViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 import SnapKit
 
 final class FeedbackFromMeDetailViewController: BaseViewController {
-
+    
     // MARK: - property
+    
+    private let backButton = BackButton(type: .system)
+    private let deleteButton: UILabel = {
+        let label = UILabel()
+        label.text = "삭제"
+        label.textColor = .red100
+        label.font = .label2
+        label.frame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        label.textAlignment = .center
+        return label
+    }()
     
     // MARK: - life cycle
     
     // MARK: - func
-
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        
+        let button = removeBarButtonItemOffset(with: backButton, offsetX: 10)
+        let backButton = makeBarButtonItem(with: button)
+        let deleteButton = makeBarButtonItem(with: deleteButton)
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.rightBarButtonItem = deleteButton
+    }
 }
