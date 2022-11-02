c0e5fb997f020a919fc772292070116674c8d728
chemi <wbsldj7645@naver.com>
2022-11-02 17:39:43 +0900

    [ADD] MemberCollectionView 레이아웃 추가 (#65)

---
+++ Maddori.Apple/Maddori.Apple/Screen/MyBox/Cell/MyBoxMemberCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyBoxMemberCollectionViewCell.swift
+//  Maddori.Apple
+//
+//  Created by Mingwan Choi on 2022/11/02.
+//
+
+import UIKit
+
+final class MyBoxMemberCollectionViewCell: BaseCollectionViewCell {
+    override func configUI() {
+        backgroundColor = .red
+    }
+}
--- Maddori.Apple/Maddori.Apple/Screen/MyBox/MyBoxViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/MyBox/MyBoxViewController.swift
@@ -11,12 +11,37 @@ import SnapKit
 
 final class MyBoxViewController: BaseViewController {
     
+    private enum Size {
+        static let horizontalPadding: CGFloat = 24
+        static let verticalPadding: CGFloat = 20
+        static let cellSize: CGFloat = 60
+        static let minimumLineSpacing: CGFloat = 16
+    }
+    
     // MARK: - property
     
     private let myFeedbackLabel: UILabel = {
         let label = UILabel()
         label.setTitleFont(text: "내가 작성한 피드백")
         return label
+    }()
+    private let collectionViewFlowLayout: UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sectionInset = UIEdgeInsets(top: Size.verticalPadding,
+                                               left: Size.horizontalPadding,
+                                               bottom: Size.verticalPadding,
+                                               right: Size.horizontalPadding)
+        flowLayout.itemSize = CGSize(width: Size.cellSize, height: Size.cellSize)
+        flowLayout.minimumLineSpacing = Size.minimumLineSpacing
+        return flowLayout
+    }()
+    private lazy var memberCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MyBoxMemberCollectionViewCell.self, forCellWithReuseIdentifier: MyBoxMemberCollectionViewCell.className)
+        return collectionView
     }()
     
     // MARK: - life cycle
@@ -27,5 +52,31 @@ final class MyBoxViewController: BaseViewController {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(20)
             $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
         }
+        
+        view.addSubview(memberCollectionView)
+        memberCollectionView.snp.makeConstraints {
+            $0.top.equalTo(myFeedbackLabel.snp.bottom).offset(7)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(100)
+        }
+    }
+}
+
+// MARK: - extension
+
+extension MyBoxViewController: UICollectionViewDelegate {
+    
+}
+
+extension MyBoxViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyBoxMemberCollectionViewCell.className, for: indexPath) as? MyBoxMemberCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
     }
 }
