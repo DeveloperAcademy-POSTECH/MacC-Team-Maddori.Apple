a6d8f3c595f563228c1f73015b300152e3fc0f04
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-11-02 23:43:15 +0900

    [ADD] init 뷰 세팅 (#42)

---
+++ Maddori.Apple/Maddori.Apple/Screen/MyReflection/FavoriteReflectionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FavoriteReflectionViewController.swift
+//  Maddori.Apple
+//
+//  Created by 이성호 on 2022/11/02.
+//
+
+import UIKit
+
+import SnapKit
+
+final class FavoriteReflectionViewController: BaseViewController {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func render() {
+        
+    }
+    
+    override func configUI() {
+        
+    }
+    
+    // MARK: - func
+}
