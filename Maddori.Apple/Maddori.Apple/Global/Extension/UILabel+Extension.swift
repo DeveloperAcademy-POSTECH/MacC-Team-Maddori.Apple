d85276721ce57a390fa4c45eaea6103b47883fc4
chemi <wbsldj7645@naver.com>
2022-11-05 19:25:04 +0900

    [CHORE] UILabel text 크기 구하는 extension 추가 (#65)

--- Maddori.Apple/Maddori.Apple/Global/Extension/UILabel+Extension.swift
+++ Maddori.Apple/Maddori.Apple/Global/Extension/UILabel+Extension.swift
@@ -53,4 +53,13 @@ extension UILabel {
         self.setLineSpacing()
         self.numberOfLines = 0
     }
+    
+    class func textSize(font: UIFont, text: String, width: CGFloat, height: CGFloat) -> CGSize {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        label.numberOfLines = 0
+        label.font = font
+        label.text = text
+        label.sizeToFit()
+        return label.frame.size
+    }
 }
