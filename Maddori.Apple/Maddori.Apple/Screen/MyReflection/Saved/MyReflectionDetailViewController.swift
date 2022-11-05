73c06d2ba0e0e25285438970e7f12a9144747b27
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-11-06 04:04:18 +0900

    [ADD] Init 커밋(#75)

---
+++ Maddori.Apple/Maddori.Apple/Screen/MyReflection/Saved/MyReflectionDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MyReflectionDetailViewController.swift
+//  Maddori.Apple
+//
+//  Created by 이성호 on 2022/11/06.
+//
+
+import UIKit
+
+import SnapKit
+
+class MyReflectionDetailViewController: BaseViewController {
+    
+    // MARK: - property
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func render() {
+        
+    }
+    
+    // MARK: - func
+}
