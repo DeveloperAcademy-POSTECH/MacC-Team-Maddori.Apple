79bfc429ab2fc09486d6977e8e7f3e51ddf0b241
Guel-git <eccyuna@gmail.com>
2022-11-06 16:55:26 +0900

    [ADD] divider & button 추가(#77)

--- Maddori.Apple/Maddori.Apple/Global/UIComponent/View/AlertView.swift
+++ Maddori.Apple/Maddori.Apple/Global/UIComponent/View/AlertView.swift
@@ -18,6 +18,8 @@ final class AlertView: UIView {
     private enum Size {
         static let alertViewWidth: CGFloat = 265
         static let alertViewHeight: CGFloat = 163
+        static let dividerWeight: CGFloat = 0.5
+        static let buttonMinimumSize: CGFloat = 44
     }
     
     var title: String? {
@@ -59,6 +61,30 @@ final class AlertView: UIView {
         label.font = .caption1
         return label
     }()
+    private let labelButtonDivider: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray300
+        return view
+    }()
+    private let buttonIntervalDivider: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray300
+        return view
+    }()
+    private let cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("취소", for: .normal)
+        button.setTitleColor(.gray500, for: .normal)
+        button.titleLabel?.textAlignment = .center
+        // action 추가
+        return button
+    }()
+    private lazy var actionButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.textAlignment = .center
+        // action 추가
+        return button
+    }()
     
     // MARK: - life cycle
     
@@ -93,11 +119,58 @@ final class AlertView: UIView {
             $0.top.equalTo(titleLabel.snp.bottom).offset(10)
             $0.centerX.equalTo(alertView.snp.centerX)
         }
+        
+        alertView.addSubview(labelButtonDivider)
+        labelButtonDivider.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(27)
+            $0.leading.trailing.equalTo(alertView)
+            $0.height.equalTo(Size.dividerWeight)
+        }
+        
+        alertView.addSubview(buttonIntervalDivider)
+        buttonIntervalDivider.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(27)
+            $0.centerX.equalTo(alertView.snp.centerX)
+            $0.width.equalTo(Size.dividerWeight)
+            $0.height.equalTo(50)
+        }
+        
+        alertView.addSubview(cancelButton)
+        cancelButton.snp.makeConstraints {
+            $0.width.greaterThanOrEqualTo(Size.buttonMinimumSize)
+            $0.height.equalTo(Size.buttonMinimumSize)
+            $0.top.equalTo(labelButtonDivider.snp.bottom).offset(3)
+            $0.trailing.equalTo(buttonIntervalDivider.snp.leading).offset(-44)
+        }
+        
+        alertView.addSubview(actionButton)
+        actionButton.snp.makeConstraints {
+            $0.width.greaterThanOrEqualTo(Size.buttonMinimumSize)
+            $0.height.equalTo(Size.buttonMinimumSize)
+            $0.top.equalTo(labelButtonDivider.snp.bottom).offset(3)
+            $0.trailing.equalTo(buttonIntervalDivider.snp.trailing).offset(44)
+        }
     }
     
     // MARK: - func
     
     private func setupAttribute() {
+        titleLabel.text = title
+        subTitleLabel.text = subTitle
         
+        switch alertType {
+        case .delete:
+            actionButton.setTitle(alertType?.rawValue, for: .normal)
+            actionButton.setTitleColor(.red100, for: .normal)
+        case .join:
+            actionButton.setTitle(alertType?.rawValue, for: .normal)
+            actionButton.setTitleColor(.gray500, for: .normal)
+            
+        // FIXME: alertType이 전달되지 않을 경우
+            
+        case .none:
+            actionButton.setTitle("확인", for: .normal)
+            actionButton.setTitleColor(.gray500, for: .normal)
+        }
     }
 }
