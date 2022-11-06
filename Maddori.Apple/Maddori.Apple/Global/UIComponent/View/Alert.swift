f5d53afcdd38478fc5cc74c39973a12ec30a0685
Guel-git <eccyuna@gmail.com>
2022-11-06 15:33:03 +0900

    [ADD] 파일 생성(#77)

---
+++ Maddori.Apple/Maddori.Apple/Global/UIComponent/View/Alert.swift
@@ -0,0 +1,20 @@
+//
+//  Alert.swift
+//  Maddori.Apple
+//
+//  Created by 김유나 on 2022/11/06.
+//
+
+import UIKit
+
+import SnapKit
+
+final class Alert: UIView {
+    
+    // MARK: - property
+    
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+}
