3dec0d0b1c5110ebac95678bac305b02b21feda4
chemi <wbsldj7645@naver.com>
2022-11-06 22:35:38 +0900

    [ADD] KeyGo Logo Image 추가 (#81)

--- Maddori.Apple/Maddori.Apple/Global/Literal/ImageLiteral.swift
+++ Maddori.Apple/Maddori.Apple/Global/Literal/ImageLiteral.swift
@@ -19,6 +19,8 @@ enum ImageLiterals {
     // MARK: - image
     
     static var imgCalendar: UIImage { .load(name: "calendar") }
+    static var imgKeygoLogo: UIImage { .load(name: "KeyGoIcon") }
+    static var imgKeygoTextLogo: UIImage { .load(name: "KeyGoTextIcon") }
 }
 
 extension UIImage {
---
+++ Maddori.Apple/Maddori.Apple/Screen/Login/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  Maddori.Apple
+//
+//  Created by Mingwan Choi on 2022/11/06.
+//
+
+import Foundation
