dded4d1ff100a6efc412d0b66420b2d072bf933e
Guel-git <eccyuna@gmail.com>
2022-11-09 10:23:44 +0900

    [CHORE] if debug & fix me 추가(#84)

--- Maddori.Apple/Maddori.Apple/Network/Member/Member.swift
+++ Maddori.Apple/Maddori.Apple/Network/Member/Member.swift
@@ -16,6 +16,7 @@ struct Member {
         self.nickname = nickname
     }
     
+    #if DEBUG
     static let mockData: [Member] = [
         Member(id: 0, nickname: "메리"),
         Member(id: 1, nickname: "이드"),
@@ -23,9 +24,12 @@ struct Member {
         Member(id: 3, nickname: "케미"),
         Member(id: 4, nickname: "호야")
     ]
+    #endif
     
     static func getTotalMemberList() -> [String] {
         var totalMemberList: [String] = []
+        
+        // FIXME: - api 로 받아온 데이터 연결하기
         for member in Member.mockData {
             totalMemberList.append(member.nickname)
         }
@@ -33,9 +37,12 @@ struct Member {
     }
     
     static func getMemberListExceptUser() -> [String] {
+        
         // FIXME: - UserDefault에서 유저 불러오기
         let userId = 2
         var memberListExceptUser: [String] = []
+        
+        // FIXME: - api 로 받아온 데이터 연결하기
         for member in Member.mockData {
             if userId != member.id {
                 memberListExceptUser.append(member.nickname)
