5b141245fee8289e39d67d62a0ea356d6fdf3c68
Hoya <sholee0508@naver.com>
2022-11-14 10:09:10 +0900

    [ADD] 커스텀 탭바 init (#91)

---
+++ Maddori.Apple/Maddori.Apple/Global/UIComponent/TabBar/CustomTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTabBarController.swift
+//  Maddori.Apple
+//
+//  Created by LeeSungHo on 2022/11/14.
+//
+
+import UIKit
+
+final class CustomTabBarController: UITabBarController {
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - func
+    
+    private func setupTabBar() {
+        
+    }
+}
