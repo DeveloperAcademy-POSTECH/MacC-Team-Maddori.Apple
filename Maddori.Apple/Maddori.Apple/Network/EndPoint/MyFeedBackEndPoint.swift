d774768543336273406a964920f33fe2454474ac
chemi <wbsldj7645@naver.com>
2022-11-17 20:20:23 +0900

    [FEAT] 특정 멤버에게 쓴 피드백 불러오기 API EndPoint 작성 (#119)

--- Maddori.Apple/Maddori.Apple/Network/EndPoint/MyFeedBackEndPoint.swift
+++ Maddori.Apple/Maddori.Apple/Network/EndPoint/MyFeedBackEndPoint.swift
@@ -9,11 +9,14 @@ import Alamofire
 
 enum MyFeedBackEndPoint {
     case fetchCurrentTeamMember(teamId: Int, userId: Int)
+    case fetchCertainMemberFeedBack(teamId: Int, memberId: Int, userId: Int)
     
     var address: String {
         switch self {
         case .fetchCurrentTeamMember(let teamId, _):
             return "\(UrlLiteral.baseUrl)/teams/\(teamId)/members"
+        case .fetchCertainMemberFeedBack(let teamId, let memberId, _):
+            return "\(UrlLiteral.baseUrl)/teams/\(teamId)/reflections/current/feedbacks/from-me?members=\(memberId)"
         }
     }
     
@@ -21,12 +24,17 @@ enum MyFeedBackEndPoint {
         switch self {
         case .fetchCurrentTeamMember:
             return .get
+        case .fetchCertainMemberFeedBack:
+            return .get
         }
     }
     
     var headers: HTTPHeaders? {
         switch self {
         case .fetchCurrentTeamMember(_, let userId):
+            let headers = ["user_id": "\(userId)"]
+            return HTTPHeaders(headers)
+        case .fetchCertainMemberFeedBack(_, _, let userId):
             let headers = ["user_id": "\(userId)"]
             return HTTPHeaders(headers)
         }
