9e6f7df8a77537bdbf1b1e597d4c332bed89b931
chemi <wbsldj7645@naver.com>
2022-11-17 23:01:23 +0900

    [CHORE] 피드백 삭제 EndPoint 추가 (#121)

--- Maddori.Apple/Maddori.Apple/Network/EndPoint/MyFeedBackEndPoint.swift
+++ Maddori.Apple/Maddori.Apple/Network/EndPoint/MyFeedBackEndPoint.swift
@@ -10,6 +10,7 @@ import Alamofire
 enum MyFeedBackEndPoint {
     case fetchCurrentTeamMember(teamId: Int, userId: Int)
     case fetchCertainMemberFeedBack(teamId: Int, memberId: Int, userId: Int)
+    case deleteFeedBack(teamId: Int, reflectionId: Int, feedBackId: Int, userId: Int)
     
     var address: String {
         switch self {
@@ -17,6 +18,8 @@ enum MyFeedBackEndPoint {
             return "\(UrlLiteral.baseUrl)/teams/\(teamId)/members"
         case .fetchCertainMemberFeedBack(let teamId, let memberId, _):
             return "\(UrlLiteral.baseUrl)/teams/\(teamId)/reflections/current/feedbacks/from-me?members=\(memberId)"
+        case .deleteFeedBack(let teamId, let reflectionId, let feedBackId, _):
+            return "\(UrlLiteral.baseUrl)/teams/\(teamId)/reflections/\(reflectionId)/feedbacks/\(feedBackId)"
         }
     }
     
@@ -26,6 +29,8 @@ enum MyFeedBackEndPoint {
             return .get
         case .fetchCertainMemberFeedBack:
             return .get
+        case .deleteFeedBack:
+            return .delete
         }
     }
     
@@ -35,6 +40,9 @@ enum MyFeedBackEndPoint {
             let headers = ["user_id": "\(userId)"]
             return HTTPHeaders(headers)
         case .fetchCertainMemberFeedBack(_, _, let userId):
+            let headers = ["user_id": "\(userId)"]
+            return HTTPHeaders(headers)
+        case .deleteFeedBack(_, _, _, let userId):
             let headers = ["user_id": "\(userId)"]
             return HTTPHeaders(headers)
         }
