55d41955816da570e8d60e4fe7a769c0905f6d95
seongmin221 <seongmin221@naver.com>
2022-11-20 18:19:56 +0900

    [CHORE] tableview emptyview added (#145)

--- Maddori.Apple/Maddori.Apple/Global/UIComponent/View/EmptyView/EmptyFeedbackView.swift
+++ Maddori.Apple/Maddori.Apple/Global/UIComponent/View/EmptyView/EmptyFeedbackView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 import SnapKit
 
-final class EmptyFeedbackView: BaseCollectionViewCell {
+final class EmptyCollectionFeedbackView: BaseCollectionViewCell {
     
     private enum Size {
         static let capsuleWidth: CGFloat = 88
@@ -36,8 +36,47 @@ final class EmptyFeedbackView: BaseCollectionViewCell {
         self.addSubview(emptyFeedbackKeyword)
         emptyFeedbackKeyword.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-//            $0.centerY.equalToSuperview().offset(-Size.capsuleYOffset)
-            $0.centerY.equalToSuperview().offset(-105)
+            $0.centerY.equalToSuperview().offset(-Size.capsuleYOffset)
+            $0.width.equalTo(Size.capsuleWidth)
+            $0.height.equalTo(Size.capsuleHeight)
+        }
+        
+        self.addSubview(emptyFeedbackLabel)
+        emptyFeedbackLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(emptyFeedbackKeyword.snp.bottom).offset(20)
+        }
+    }
+}
+
+final class EmptyTableFeedbackView: BaseTableViewCell {
+    
+    private enum Size {
+        static let capsuleWidth: CGFloat = 88
+        static let capsuleHeight: CGFloat = 54
+        static let capsuleYOffset: CGFloat = 20
+        static let labelYOffset: CGFloat = 25
+    }
+    
+    // MARK: - property
+    
+    private let emptyFeedbackKeyword = EmptyFeedbackKeyword()
+    lazy var emptyFeedbackLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.font = .body3
+        label.textColor = .gray700
+        return label
+    }()
+    
+    // MARK: - life cycle
+    
+    override func render() {
+        self.addSubview(emptyFeedbackKeyword)
+        emptyFeedbackKeyword.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-Size.capsuleYOffset)
             $0.width.equalTo(Size.capsuleWidth)
             $0.height.equalTo(Size.capsuleHeight)
         }
