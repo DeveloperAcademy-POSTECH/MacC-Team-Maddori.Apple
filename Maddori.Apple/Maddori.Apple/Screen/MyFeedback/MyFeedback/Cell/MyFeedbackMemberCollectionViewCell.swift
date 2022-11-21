074633da73ecde2007ca2f7f2ef26a5a498fd0bb
seongmin221 <seongmin221@naver.com>
2022-11-21 20:06:05 +0900

    [CHORE] 코드리뷰 반영 (#148)

--- Maddori.Apple/Maddori.Apple/Screen/MyFeedback/MyFeedback/Cell/MyFeedbackMemberCollectionViewCell.swift
+++ Maddori.Apple/Maddori.Apple/Screen/MyFeedback/MyFeedback/Cell/MyFeedbackMemberCollectionViewCell.swift
@@ -34,6 +34,8 @@ final class MyFeedbackMemberCollectionViewCell: BaseCollectionViewCell {
         let label = UILabel()
         label.font = .body2
         label.textColor = .gray400
+        label.lineBreakMode = .byTruncatingTail
+        label.textAlignment = .center
         return label
     }()
     
@@ -52,6 +54,7 @@ final class MyFeedbackMemberCollectionViewCell: BaseCollectionViewCell {
         self.addSubview(memberNameLabel)
         memberNameLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(4)
         }
     }
     
@@ -74,13 +77,6 @@ final class MyFeedbackMemberCollectionViewCell: BaseCollectionViewCell {
     }
     
     func setMemberName(name: String) {
-        if name.count >= 3 {
-            var reducedName = name
-            let range = name.index(name.startIndex, offsetBy: 2)..<name.endIndex
-            reducedName.replaceSubrange(range, with: "..")
-            memberNameLabel.text = reducedName
-        } else {
-            memberNameLabel.text = name
-        }
+        memberNameLabel.text = name
     }
 }
