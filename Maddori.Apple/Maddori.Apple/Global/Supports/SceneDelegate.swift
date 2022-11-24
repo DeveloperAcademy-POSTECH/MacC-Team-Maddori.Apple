d2742bcabed74662ed304a57bdb5033d9e17ed82
Guel-git <eccyuna@gmail.com>
2022-11-24 15:14:48 +0900

    [CHORE] SceneDelegate 수정(#170)

--- Maddori.Apple/Maddori.Apple/Global/Supports/SceneDelegate.swift
+++ Maddori.Apple/Maddori.Apple/Global/Supports/SceneDelegate.swift
@@ -18,7 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        UserDefaultHandler.clearAllData()
         let isLogined = UserDefaultStorage.isLogin
         var rootViewController: UIViewController
         if isLogined {
