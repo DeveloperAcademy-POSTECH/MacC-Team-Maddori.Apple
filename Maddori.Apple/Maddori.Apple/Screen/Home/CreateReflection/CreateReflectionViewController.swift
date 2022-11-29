97b346d868726c60cfb6aab149b5eebcafd5fca8
Hoya <sholee0508@naver.com>
2022-11-29 11:50:15 +0900

    [FEAT] 메인버튼 키보드 위치에 따라 올라오도록 변경 (#187)

--- Maddori.Apple/Maddori.Apple/Screen/Home/CreateReflection/CreateReflectionViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/CreateReflection/CreateReflectionViewController.swift
@@ -84,6 +84,7 @@ final class CreateReflectionViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAddReflection()
+        setupNotificationCenter()
     }
     
     override func render() {
@@ -131,6 +132,11 @@ final class CreateReflectionViewController: BaseViewController {
     }
     
     // MARK: - setup
+    
+    private func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
     
     private func setupAddReflection() {
         let action = UIAction { [weak self] _ in
@@ -181,6 +187,22 @@ final class CreateReflectionViewController: BaseViewController {
         guard let combinedDate = calendar.date(from: combinedDateComponents) else { return Date() }
         
         return combinedDate
+    }
+    
+    // MARK: - selector
+    
+    @objc private func keyboardWillShow(notification:NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.mainButton.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height + 10)
+            })
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification:NSNotification) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.mainButton.transform = .identity
+        })
     }
     
     // MARK: - api
