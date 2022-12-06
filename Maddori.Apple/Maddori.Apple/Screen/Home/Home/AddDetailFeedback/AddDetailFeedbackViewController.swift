3b435fb48623285f78dd47166827a71afc94ddfd
Hoya <sholee0508@naver.com>
2022-12-06 14:55:42 +0900

    [FEAT] Close버튼 생성 및 액션 연결 (#209)

--- Maddori.Apple/Maddori.Apple/Screen/Home/Home/AddDetailFeedback/AddDetailFeedbackViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/Home/AddDetailFeedback/AddDetailFeedbackViewController.swift
@@ -13,8 +13,29 @@ final class AddDetailFeedbackViewController: BaseViewController {
     
     // MARK: - property
     
+    private let closeButton = CloseButton()
+    
     // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCloseButton()
+    }
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        
+        let closeButton = makeBarButtonItem(with: closeButton)
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = closeButton
+    }
     
     // MARK: - func
-    
+    private func setupCloseButton() {
+        let action = UIAction { [weak self] _ in
+            self?.dismiss(animated: true)
+        }
+        closeButton.addAction(action, for: .touchUpInside)
+    }
 }
