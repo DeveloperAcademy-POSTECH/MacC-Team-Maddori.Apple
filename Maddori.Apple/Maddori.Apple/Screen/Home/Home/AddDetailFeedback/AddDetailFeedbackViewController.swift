d9c3b454554f5950521be04d9c8c53eab3976e0b
Hoya <sholee0508@naver.com>
2022-12-06 14:39:05 +0900

    [FEAT] AddDetailFeedbackViewController 파일 생성  (#209)

---
+++ Maddori.Apple/Maddori.Apple/Screen/Home/Home/AddDetailFeedback/AddDetailFeedbackViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddDetailFeedbackViewController.swift
+//  Maddori.Apple
+//
+//  Created by LeeSungHo on 2022/12/06.
+//
+
+import UIKit
+
+import SnapKit
+
+final class AddDetailFeedbackViewController: BaseViewController {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+    
+}
