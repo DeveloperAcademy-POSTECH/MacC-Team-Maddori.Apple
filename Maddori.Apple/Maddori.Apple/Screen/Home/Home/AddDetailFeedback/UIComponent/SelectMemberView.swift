7da5b6acbcd08ddc5d20eb12930ed11ae2d04501
Hoya <sholee0508@naver.com>
2022-12-10 14:44:38 +0900

    [CHORE] 코드리뷰 반영 - 한줄 삭제 (#20()

--- Maddori.Apple/Maddori.Apple/Screen/Home/Home/AddDetailFeedback/UIComponent/SelectMemberView.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/Home/AddDetailFeedback/UIComponent/SelectMemberView.swift
@@ -77,7 +77,6 @@ final class SelectMemberView: UIStackView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
     // MARK: - func
     private func render() {
         
