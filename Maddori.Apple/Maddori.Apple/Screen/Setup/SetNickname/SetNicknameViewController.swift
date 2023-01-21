37ceba0b9d9dcd7b5abf1571ef4099ded807a3b8
Guel-git <eccyuna@gmail.com>
2023-01-21 17:50:15 +0900

    [ADD] navbar & titleLabel 생성(#278)

--- Maddori.Apple/Maddori.Apple/Screen/Setup/SetNickname/SetNicknameViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Setup/SetNickname/SetNicknameViewController.swift
@@ -8,93 +8,95 @@
 import UIKit
 
 import Alamofire
+import SnapKit
 
-final class SetNicknameViewController: BaseTextFieldViewController {
+final class SetNicknameViewController: BaseViewController {
     
-    override var titleText: String {
-        get {
-            return TextLiteral.setNicknameViewControllerTitleLabel
-        }
-        set {
-            super.titleText = newValue
-        }
-    }
+    var teamName = "맛쟁이사과처럼세글자"
     
-    override var placeholderText: String {
-        get {
-            return TextLiteral.setNicknameViewControllerNicknameTextFieldPlaceHolder
-        }
-        set {
-            super.placeholderText = newValue
-        }
-    }
+    // MARK: - property
     
-    override var maxLength: Int {
-        get {
-            return 6
+    private lazy var backButton: BackButton = {
+        let button = BackButton()
+        let action = UIAction { [weak self] _ in
+            self?.navigationController?.popViewController(animated: true)
         }
-        
-        set {
-            super.maxLength = newValue
-        }
-    }
+        button.addAction(action, for: .touchUpInside)
+        return button
+    }()
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = teamName + "에서\n사용할 프로필을 작성해 주세요"
+        label.font = .title
+        label.textColor = .black100
+        label.numberOfLines = 0
+        label.setLineSpacingWithColorApplied(amount: 4, colorTo: teamName, with: .blue200)
+        return label
+    }()
     
-    override var buttonText: String {
-        get {
-            return TextLiteral.doneButtonTitle
-        }
-        set {
-            super.buttonText = newValue
-        }
-    }
+    // MARK: - life cycle
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupDoneButton()
+    //    override func viewDidLoad() {
+    //        super.viewDidLoad()
+    //        setupDoneButton()
+    //    }
+    
+    override func render() {
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
     }
     
     override func setupNavigationBar() {
         super.setupNavigationBar()
+        
+        let button = removeBarButtonItemOffset(with: backButton, offsetX: 10)
+        let backButton = makeBarButtonItem(with: button)
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.leftBarButtonItem = backButton
     }
     
-    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        return false
-    }
+    //    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    //        return false
+    //    }
     
     // MARK: - func
     
-    override func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
+    //    override func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    //        textField.resignFirstResponder()
+    //        return true
+    //    }
     
-    private func setupDoneButton() {
-        let action = UIAction { [weak self] _ in
-            guard let nickname = self?.kigoTextField.text else { return }
-            self?.dispatchUserLogin(type: .dispatchLogin(LoginDTO(username: nickname)))
-            self?.kigoTextField.resignFirstResponder()
-        }
-        super.doneButton.addAction(action, for: .touchUpInside)
-    }
+    //    private func setupDoneButton() {
+    //        let action = UIAction { [weak self] _ in
+    //            guard let nickname = self?.kigoTextField.text else { return }
+    //            self?.dispatchUserLogin(type: .dispatchLogin(LoginDTO(username: nickname)))
+    //            self?.kigoTextField.resignFirstResponder()
+    //        }
+    //        super.doneButton.addAction(action, for: .touchUpInside)
+    //    }
     
     // MARK: - api
     
-    private func dispatchUserLogin(type: SetupEndPoint<LoginDTO>) {
-        AF.request(type.address,
-                   method: type.method,
-                   parameters: type.body,
-                   encoder: JSONParameterEncoder.default,
-                   headers: type.headers
-        ).responseDecodable(of: BaseModel<JoinMemberResponse>.self) { [weak self] response in
-            guard let self else { return }
-            switch response.result {
-            case .success:
-                guard let nickname = self.kigoTextField.text else { return }
-                UserDefaultHandler.setNickname(nickname: nickname)
-                self.navigationController?.pushViewController(JoinTeamViewController(), animated: true)
-            case .failure:
-                self.makeAlert(title: TextLiteral.setNicknameViewControllerAlertTitle, message: TextLiteral.setNicknameControllerAlertMessage)
-            }
-        }
-    }
+    //    private func dispatchUserLogin(type: SetupEndPoint<LoginDTO>) {
+    //        AF.request(type.address,
+    //                   method: type.method,
+    //                   parameters: type.body,
+    //                   encoder: JSONParameterEncoder.default,
+    //                   headers: type.headers
+    //        ).responseDecodable(of: BaseModel<JoinMemberResponse>.self) { [weak self] response in
+    //            guard let self else { return }
+    //            switch response.result {
+    //            case .success:
+    //                guard let nickname = self.kigoTextField.text else { return }
+    //                UserDefaultHandler.setNickname(nickname: nickname)
+    //                self.navigationController?.pushViewController(JoinTeamViewController(), animated: true)
+    //            case .failure:
+    //                self.makeAlert(title: TextLiteral.setNicknameViewControllerAlertTitle, message: TextLiteral.setNicknameControllerAlertMessage)
+    //            }
+    //        }
+    //    }
 }
