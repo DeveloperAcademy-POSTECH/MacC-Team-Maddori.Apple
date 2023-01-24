42ce1c186b13f5edf0646c8e361eb90497943d35
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-01-24 17:46:03 +0900

    [CHORE] 모달 위치 확인하기 (#280)

--- Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
@@ -12,18 +12,62 @@ import SnapKit
 final class TeamManageViewController: BaseViewController {
     
     // MARK: - property
+    private let label = UILabel()
+    private let manageTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
     
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableViewRegisterCell()
+        setupTableViewDelegateDataSource()
     }
     
-    // MARK: - func
     override func render() {
+        label.text = "테스트느트트트"
+        view.addSubview(label)
+        label.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(40)
+            $0.left.equalToSuperview().inset(40)
+        }
         
+        view.addSubview(manageTableView)
+        manageTableView.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom)
+            $0.width.equalTo(200)
+            $0.height.equalTo(600)
+        }
     }
+    
     override func configUI() {
         super.configUI()
         view.backgroundColor = .red
     }
+    
+    // MARK: - func
+    private func setupTableViewRegisterCell() {
+        manageTableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.className)
+    }
+    
+    private func setupTableViewDelegateDataSource() {
+        manageTableView.delegate = self
+        manageTableView.dataSource = self
+    }
+}
+
+extension TeamManageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.className, for: indexPath) as? UITableViewCell else { return UITableViewCell() }
+        return cell
+    }
+}
+
+extension TeamManageViewController: UITableViewDelegate {
+    
 }
