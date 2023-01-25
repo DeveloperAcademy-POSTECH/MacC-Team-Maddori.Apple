f53aa339da36d10b979151a72d54d8f324463024
chemi <wbsldj7645@naver.com>
2023-01-25 17:58:04 +0900

    [ADD] 팀 정보 수정 버튼 추가 (#282)

--- Maddori.Apple/Maddori.Apple/Screen/Home/TeamDetail/TeamDetailViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/TeamDetail/TeamDetailViewController.swift
@@ -22,8 +22,21 @@ final class TeamDetailViewController: BaseViewController {
         label.text = "맛쟁이 사과처럼"
         return label
     }()
+    private let editButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("편집", for: .normal)
+        button.titleLabel?.font = .label2
+        button.setTitleColor(.gray500, for: .normal)
+        button.setUnderline()
+        return button
+    }()
     
     // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupEditButton()
+    }
     
     override func configUI() {
         super.configUI()
@@ -36,6 +49,13 @@ final class TeamDetailViewController: BaseViewController {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(20)
             $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
         }
+        
+        view.addSubview(editButton)
+        editButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.width.height.equalTo(44)
+        }
     }
     
     override func setupNavigationBar() {
@@ -45,5 +65,15 @@ final class TeamDetailViewController: BaseViewController {
         let backButton = makeBarButtonItem(with: button)
         
         navigationItem.leftBarButtonItem = backButton
+    }
+    
+    // MARK: - func
+    
+    private func setupEditButton() {
+        let action = UIAction { _ in
+            // FIXME: - 방 정보 수정 view 연결
+            print("action")
+        }
+        self.editButton.addAction(action, for: .touchUpInside)
     }
 }
