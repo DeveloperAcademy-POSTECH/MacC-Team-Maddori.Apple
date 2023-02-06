f0d36a05ba847a758b5101713b6ee95c2a924ebb
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-02-06 13:42:28 +0900

    [CHORE] 디바이더 뷰 생성 (#280)

--- Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
@@ -12,6 +12,15 @@ import SnapKit
 final class TeamManageViewController: BaseViewController {
     
     // MARK: - property
+    private let tempView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray100
+        return view
+    }()
     
     // MARK: - life cycle
     override func viewDidLoad() {
@@ -19,6 +28,19 @@ final class TeamManageViewController: BaseViewController {
     }
     
     override func render() {
+        view.addSubview(tempView)
+        tempView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(275) // FIXME: 수치 바꿀것
+            $0.top.equalToSuperview()
+        }
+        
+        view.addSubview(dividerView)
+        dividerView.snp.makeConstraints {
+            $0.top.equalTo(tempView.snp.bottom)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(6)
+        }
     }
     
     override func configUI() {
