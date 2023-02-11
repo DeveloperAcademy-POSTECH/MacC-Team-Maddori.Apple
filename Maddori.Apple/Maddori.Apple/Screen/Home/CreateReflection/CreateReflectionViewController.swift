4d6b93bb6952162bb5e08e478793bb65760fdb2a
seongmin221 <seongmin221@naver.com>
2023-02-11 17:33:14 +0900

    [CHORE] 코드리뷰 반영 (#295)

--- Maddori.Apple/Maddori.Apple/Screen/Home/CreateReflection/CreateReflectionViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/CreateReflection/CreateReflectionViewController.swift
@@ -54,11 +54,17 @@ final class CreateReflectionViewController: BaseViewController {
     }()
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.setTitleFont(text: TextLiteral.createReflectionViewControllerTitle)
+        label.setTitleFont(text: isEditReflection ? TextLiteral.editReflectionViewControllerTitle : TextLiteral.createReflectionViewControllerTitle)
         label.textColor = .black100
         return label
     }()
-    private let reflectionNameView = ReflectionNameView()
+    private lazy var reflectionNameView: ReflectionNameView = {
+        let nameView = ReflectionNameView()
+        if isEditReflection {
+            nameView.nameTextField.text = reflectionTitle
+        }
+        return nameView
+    }()
     private let reflectionDateLabel: UILabel = {
         let label = UILabel()
         label.text = TextLiteral.createReflectionViewControllerDateLabel
@@ -73,6 +79,9 @@ final class CreateReflectionViewController: BaseViewController {
         }
         let hideKeyboardAction = UIAction { [weak self] _ in
             self?.view.endEditing(true)
+        }
+        if isEditReflection, let date = reflectionDate {
+            picker.date = date
         }
         picker.datePickerMode = .date
         picker.locale = Locale(identifier: "ko_KR")
@@ -92,6 +101,9 @@ final class CreateReflectionViewController: BaseViewController {
         let hideKeyboardAction = UIAction { [weak self] _ in
             self?.view.endEditing(true)
         }
+        if isEditReflection, let date = reflectionDate {
+            picker.date = date
+        }
         picker.datePickerMode = .time
         picker.locale = Locale(identifier: "ko_KR")
         picker.preferredDatePickerStyle = .inline
@@ -104,7 +116,7 @@ final class CreateReflectionViewController: BaseViewController {
     }()
     private lazy var mainButton: MainButton = {
         let button = MainButton()
-        button.title = TextLiteral.createReflectionViewControllerButtonText
+        button.title = isEditReflection ? TextLiteral.editReflectionViewControllerButtonText : TextLiteral.createReflectionViewControllerButtonText
         return button
     }()
     
@@ -116,12 +128,12 @@ final class CreateReflectionViewController: BaseViewController {
         setupNotificationCenter()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if isEditReflection {
-            setupEditReflection()
-        }
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        if isEditReflection {
+//            setupEditReflection()
+//        }
+//    }
     
     override func render() {
         view.addSubview(titleLabel)
@@ -195,9 +207,10 @@ final class CreateReflectionViewController: BaseViewController {
         mainButton.addAction(action, for: .touchUpInside)
     }
     
+    
     private func setupEditReflection() {
-        titleLabel.setTitleFont(text: TextLiteral.editReflectionViewControllerTitle)
-        mainButton.title = TextLiteral.editReflectionViewControllerButtonText
+//        titleLabel.setTitleFont(text: TextLiteral.editReflectionViewControllerTitle)
+//        mainButton.title = TextLiteral.editReflectionViewControllerButtonText
         guard let title = reflectionTitle, let date = reflectionDate else { return }
         reflectionNameView.nameTextField.text = title
         datePicker.date = date
@@ -208,6 +221,7 @@ final class CreateReflectionViewController: BaseViewController {
         navigationItem.leftBarButtonItem = closeButton
         navigationItem.rightBarButtonItem = deleteButton
     }
+    
     
     // MARK: - func
     
