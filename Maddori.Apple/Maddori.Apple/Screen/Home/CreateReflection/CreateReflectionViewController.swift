34557c32c7a2f0c65c194f8a9cd8e542db157d62
seongmin221 <seongmin221@naver.com>
2023-02-12 22:39:07 +0900

    [CHORE] restore previous commit (#295)

--- Maddori.Apple/Maddori.Apple/Screen/Home/CreateReflection/CreateReflectionViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/CreateReflection/CreateReflectionViewController.swift
@@ -114,7 +114,7 @@ final class CreateReflectionViewController: BaseViewController {
         picker.addAction(hideKeyboardAction, for: .editingDidBegin)
         return picker
     }()
-    private let mainButton: MainButton = {
+    private lazy var mainButton: MainButton = {
         let button = MainButton()
         button.title = isEditReflection ? TextLiteral.editReflectionViewControllerButtonText : TextLiteral.createReflectionViewControllerButtonText
         return button
