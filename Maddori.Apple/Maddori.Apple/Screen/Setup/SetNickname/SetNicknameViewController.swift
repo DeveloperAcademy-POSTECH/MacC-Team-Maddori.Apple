42fdad06060fb115234928b92b082f7112ff10e4
Guel-git <eccyuna@gmail.com>
2023-02-28 17:38:29 +0900

    [ADD] 갤러리 - CreateTeam 성공(#311)

--- Maddori.Apple/Maddori.Apple/Screen/Setup/SetNickname/SetNicknameViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Setup/SetNickname/SetNicknameViewController.swift
@@ -289,7 +289,7 @@ final class SetNicknameViewController: BaseViewController {
         guard let nickname = nicknameTextField.text else { return }
         guard let role = roleTextField.text else { return }
         if UserDefaultStorage.teamId == 0 {
-//            let dto = CreateTeamDTO(team_name: teamName, nickname: nickname, role: role, profile_image: nil)
+            //            let dto = CreateTeamDTO(team_name: teamName, nickname: nickname, role: role, profile_image: nil)
             dispatchCreateTeam(type: .dispatchCreateTeam, teamName: teamName, nickname: nickname, role: role)
         } else {
             //            let dto = JoinTeamDTO(nickname: nickname, role: role, profile_image: nil)
@@ -359,45 +359,38 @@ final class SetNicknameViewController: BaseViewController {
         AF.upload(multipartFormData: { multipartFormData in
             let teamInfo: Dictionary = ["team_name": teamName, "nickname": nickname, "role": role]
             for (key, value) in teamInfo {
+                if let value = value {
                     multipartFormData.append("\(value)".data(using: .utf8)!, withName: key, mimeType: "text/plain")
-                    print(key, value)
+                }
             }
             if let profileURL = self.profileURL {
                 multipartFormData.append(profileURL, withName: "profile_image", fileName: ".png", mimeType: "image/png")
             }
-        }, to: type.address, method: type.method, headers: type.headers).response { response in
-            dump(response)
+        }, to: type.address, method: type.method, headers: type.headers).responseDecodable(of: BaseModel<CreateTeamResponse>.self) { json in
+            if let json = json.value {
+                dump(json)
+                guard let teamId = json.detail?.id else { return }
+                UserDefaultHandler.setTeamId(teamId: teamId)
+                DispatchQueue.main.async {
+                    if let invitationCode = json.detail?.team?.invitationCode {
+                        self.pushInvitationCodeViewController(invitationCode: invitationCode)
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.makeAlert(title: TextLiteral.setNicknameViewControllerCreateTeamAlertTitle, message: TextLiteral.setNicknameViewControllerAlertMessage)
+                }
+            }
         }
-//        AF.request(type.address,
-//                   method: type.method,
-//                   parameters: type.body,
-//                   encoder: JSONParameterEncoder.default,
-//                   headers: type.headers
-//        ).responseDecodable(of: BaseModel<CreateTeamResponse>.self) { json in
-//            if let json = json.value {
-//                dump(json)
-//                guard let teamId = json.detail?.id else { return }
-//                UserDefaultHandler.setTeamId(teamId: teamId)
-//                DispatchQueue.main.async {
-//                    if let invitationCode = json.detail?.team?.invitationCode {
-//                        self.pushInvitationCodeViewController(invitationCode: invitationCode)
-//                    }
-//                }
-//            } else {
-//                DispatchQueue.main.async {
-//                    self.makeAlert(title: TextLiteral.setNicknameViewControllerCreateTeamAlertTitle, message: TextLiteral.setNicknameViewControllerAlertMessage)
-//                }
-//            }
-//        }
     }
-
+    
     private func dispatchJoinTeam(type: SetupEndPoint<EncodeDTO>, nickname: String, role: String?) {
         let profileInfo: Dictionary = ["nickname": nickname, "role": role]
         
         AF.upload(multipartFormData: { multipartFormData in
             for (key, value) in profileInfo {
-                    multipartFormData.append("\(value)".data(using: .utf8)!, withName: key, mimeType: "text/plain")
-                    print(key, value)
+                multipartFormData.append("\(value)".data(using: .utf8)!, withName: key, mimeType: "text/plain")
+                print(key, value)
             }
             if let profileURL = self.profileURL {
                 multipartFormData.append(profileURL, withName: "profile_image", fileName: "\(nickname).jpg", mimeType: "image/jpg")
