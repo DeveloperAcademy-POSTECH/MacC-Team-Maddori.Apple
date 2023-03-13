ca3e8bd89c1ffdc4fa57864294793974369b075e
chemi <wbsldj7645@naver.com>
2023-03-13 17:04:22 +0900

    [CHORE] 주석 제거 (#307)

--- Maddori.Apple/Maddori.Apple/Screen/Home/TeamDetail/UIComponent/FullDividerView.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/TeamDetail/UIComponent/FullDividerView.swift
@@ -11,29 +11,12 @@ import SnapKit
 
 final class FullDividerView: UIView {
     
-    // MARK: - property
-    
-//    let dividerView: UIView = {
-//        let view = UIView()
-//        view.backgroundColor = .gray300
-//        return view
-//    }()
-    
     // MARK: - life cycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .gray300
-//        render()
     }
     
     required init?(coder: NSCoder) { nil }
-    
-//    private func render() {
-//        self.addSubview(dividerView)
-//        dividerView.snp.makeConstraints {
-//            $0.edges.equalToSuperview()
-//            $0.height.equalTo(4)
-//        }
-//    }
 }
