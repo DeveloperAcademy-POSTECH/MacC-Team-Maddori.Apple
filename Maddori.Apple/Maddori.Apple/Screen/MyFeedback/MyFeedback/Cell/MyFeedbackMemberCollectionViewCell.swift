efcf1715be8aea18c8334f147512fe088a0eed2b
seongmin221 <seongmin221@naver.com>
2023-03-15 20:52:25 +0900

    [CHORE] 코드리뷰 반영 (#316)

--- Maddori.Apple/Maddori.Apple/Screen/MyFeedback/MyFeedback/Cell/MyFeedbackMemberCollectionViewCell.swift
+++ Maddori.Apple/Maddori.Apple/Screen/MyFeedback/MyFeedback/Cell/MyFeedbackMemberCollectionViewCell.swift
@@ -94,7 +94,7 @@ final class MyFeedbackMemberCollectionViewCell: BaseCollectionViewCell {
     }
     
     func setMemberProfileImage(from path: String?) {
-        if let path {
+        if let pathg {
             let fullImagePath = UrlLiteral.iamgeBaseUrl + path
             profileImageView.load(from: fullImagePath)
             
