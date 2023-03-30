f29afaa8434eb4aa337bd33408cd5e1af9d7540a
chemi <wbsldj7645@naver.com>
2023-03-30 17:48:05 +0900

    [CHORE] 팀 정보 API 추가 (#326)

--- Maddori.Apple/Maddori.Apple/Network/EndPoint/TeamDetailEndPoint.swift
+++ Maddori.Apple/Maddori.Apple/Network/EndPoint/TeamDetailEndPoint.swift
@@ -9,17 +9,24 @@ import Alamofire
 
 enum TeamDetailEndPoint<T: Encodable>: EndPointable {
     case fetchTeamMember
+    case fetchTeamInformation
     
     var address: String {
         switch self {
         case .fetchTeamMember:
             return "\(UrlLiteral.baseUrl2)/teams/\(UserDefaultStorage.teamId)/members"
+            
+        case .fetchTeamInformation:
+            return "\(UrlLiteral.baseUrl2)/teams/\(UserDefaultStorage.teamId)"
         }
     }
     
     var method: Alamofire.HTTPMethod {
         switch self {
         case .fetchTeamMember:
+            return .get
+            
+        case .fetchTeamInformation:
             return .get
         }
     }
@@ -28,12 +35,22 @@ enum TeamDetailEndPoint<T: Encodable>: EndPointable {
         switch self {
         case .fetchTeamMember:
             return nil
+            
+        case .fetchTeamInformation:
+            return nil
         }
     }
     
     var headers: Alamofire.HTTPHeaders? {
         switch self {
         case .fetchTeamMember:
+            let headers = [
+                "access_token": "\(UserDefaultStorage.accessToken)",
+                "refresh_token": "\(UserDefaultStorage.refreshToken)"
+            ]
+            return HTTPHeaders(headers)
+            
+        case .fetchTeamInformation:
             let headers = [
                 "access_token": "\(UserDefaultStorage.accessToken)",
                 "refresh_token": "\(UserDefaultStorage.refreshToken)"
