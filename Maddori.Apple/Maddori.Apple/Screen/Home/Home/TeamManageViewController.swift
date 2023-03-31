8a01c1e2a0214c45af92735b0080cc77aed9a784
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-03-31 20:55:43 +0900

    [CHORE] @available 코드 추가 (#320)

--- Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
@@ -59,6 +59,7 @@ final class TeamManageViewController: BaseViewController {
         super.init()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
