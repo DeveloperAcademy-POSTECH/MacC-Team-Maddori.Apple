561f6f9b1a4a93b5fe611118433fa737418a4e15
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-04-07 16:20:54 +0900

    [CHORE] 코드리뷰 반영 (#320)

--- Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/Home/TeamManageViewController.swift
@@ -59,9 +59,7 @@ final class TeamManageViewController: BaseViewController {
     }
     
     @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) { nil }
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- Maddori.Apple/Maddori.Apple/Screen/Setup/JoinTeam/JoinTeamViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Setup/JoinTeam/JoinTeamViewController.swift
@@ -98,9 +98,7 @@ final class JoinTeamViewController: BaseTextFieldViewController {
     }
     
     @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) { nil }
     
     override func viewDidLoad() {
         super.viewDidLoad()
