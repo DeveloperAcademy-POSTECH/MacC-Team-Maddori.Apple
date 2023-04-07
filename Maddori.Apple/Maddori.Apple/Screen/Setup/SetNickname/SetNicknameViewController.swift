5f087b02c080391174134490ca128dcc3177d379
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-04-07 16:23:43 +0900

    [CHORE] 코드리뷰 반영 (#320)

--- Maddori.Apple/Maddori.Apple/Screen/Setup/SetNickname/SetNicknameViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Setup/SetNickname/SetNicknameViewController.swift
@@ -120,9 +120,7 @@ final class SetNicknameViewController: BaseViewController {
     }
     
     @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) { nil }
     
     override func viewDidLoad() {
         super.viewDidLoad()
