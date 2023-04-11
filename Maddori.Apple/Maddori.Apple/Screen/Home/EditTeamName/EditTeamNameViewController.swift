d18b904ec9c35124b55b0693c84b561abc262f7c
chemi <wbsldj7645@naver.com>
2023-04-11 15:23:57 +0900

    [ADD] 팀 이름 수정 view UI 완성 (#339)

--- Maddori.Apple/Maddori.Apple/Screen/Home/EditTeamName/EditTeamNameViewController.swift
+++ Maddori.Apple/Maddori.Apple/Screen/Home/EditTeamName/EditTeamNameViewController.swift
@@ -5,4 +5,71 @@
 //  Created by Mingwan Choi on 2023/04/11.
 //
 
-import Foundation
+import UIKit
+
+final class EditTeamNameViewController: BaseTextFieldViewController {
+    override var titleText: String {
+        get {
+            return TextLiteral.editTeamLabelText
+        }
+        
+        set {
+            super.titleText = newValue
+        }
+    }
+    
+    override var placeholderText: String {
+        get {
+            // FIXME: - place holder의 내용이 피그마에 없음.
+            return "수정할 팀 이름을 입력해 주세요."
+        }
+        
+        set {
+            super.placeholderText = newValue
+        }
+    }
+    
+    override var maxLength: Int {
+        get {
+            return 10
+        }
+        
+        set {
+            super.maxLength = newValue
+        }
+    }
+    
+    override var buttonText: String {
+        get {
+            return TextLiteral.doneButtonComplete
+        }
+        
+        set {
+            super.buttonText = newValue
+        }
+    }
+    
+    // MARK: - property
+    
+    private lazy var backButton: BackButton = {
+        let button = BackButton()
+        let action = UIAction { [weak self] _ in
+            self?.navigationController?.popViewController(animated: true)
+        }
+        button.addAction(action, for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - life cycle
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        
+        let button = removeBarButtonItemOffset(with: backButton, offsetX: 10)
+        let backButton = makeBarButtonItem(with: button)
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.leftBarButtonItem = backButton
+    }
+}
