d6db2745ad8147a4b6ddb7ab1b32f05a5672644d
Guel-git <eccyuna@gmail.com>
2023-05-01 19:27:13 +0900

    [ADD] apple login response 정리(#350)

--- Maddori.Apple/Maddori.Apple/Network/Response/AppleLoginResponse.swift
+++ Maddori.Apple/Maddori.Apple/Network/Response/AppleLoginResponse.swift
@@ -11,12 +11,22 @@ struct AppleLoginResponse: Decodable {
     let created: Bool?
     let accessToken: String?
     let refreshToken: String?
-    let user: UserResponse?
+    let user: LoginUserResponse?
     
     enum CodingKeys: String, CodingKey {
         case created
         case accessToken = "access_token"
         case refreshToken = "refresh_token"
         case user
+    }
+}
+
+struct LoginUserResponse: Decodable {
+    let userId: Int?
+    let teamId: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case teamId = "team_id"
     }
 }
