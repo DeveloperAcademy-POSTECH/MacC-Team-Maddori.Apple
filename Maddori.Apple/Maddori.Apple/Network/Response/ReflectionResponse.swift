cae55e1a22eb4314dab52f54e754c429601d4b92
Guel-git <eccyuna@gmail.com>
2023-05-01 23:09:21 +0900

    [CHORE] Add/DeleteReflectionResponse 하나로 합치기(#350)

--- Maddori.Apple/Maddori.Apple/Network/Response/ReflectionResponse.swift
+++ Maddori.Apple/Maddori.Apple/Network/Response/ReflectionResponse.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct ReflectionResponse: Decodable {
-    // MARK: - endInProgressReflection
+    // MARK: - patchReflectionDetail, deleteReflectionDetail, endInProgressReflection
     let id: Int?
     let reflectionName: String?
     let date: String?
