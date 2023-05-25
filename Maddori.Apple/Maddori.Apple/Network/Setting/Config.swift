fb16393101b67a98ff6ce3de34b54c3d07eb2146
seongmin221 <seongmin221@naver.com>
2023-05-25 22:57:34 +0900

    [CHORE] config added for info.plist (#386)

---
+++ Maddori.Apple/Maddori.Apple/Network/Setting/Config.swift
@@ -0,0 +1,34 @@
+//
+//  Config.swift
+//  Maddori.Apple
+//
+//  Created by 이성민 on 2023/05/25.
+//
+
+import Foundation
+
+enum Config {
+    enum Keys {
+        enum Plist {
+            static let baseUrl = "BASE_URL"
+        }
+    }
+    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot be found.")
+        }
+        return dict
+    }()
+    
+}
+
+extension Config {
+    
+    static let baseUrl: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.baseUrl] as? String
+        else { fatalError("baseUrl not fetched from info.plist") }
+        return key
+    }()
+    
+}
