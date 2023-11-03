652dd7c8a75d1ae2ed9e1ac6bf65147624306293
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-11-03 23:38:30 +0900

    [FEAT] ViewModel 생성 (#397)

---
+++ Maddori.Apple/Maddori.Apple/Screen/Setup/InvitationCode/ViewModel/InvitationCodeViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  InvitationCodeViewModel.swift
+//  Maddori.Apple
+//
+//  Created by 이성호 on 11/3/23.
+//
+
+import Foundation
+
+import RxSwift
+
+final class InvitationCodeViewModel {
+    
+    // MARK: - property
+    
+    // MARK: - init
+    
+    // MARK: - func
+}
