e6591eff48fe430ecc4947e0a87ea943be444980
seongmin221 <seongmin221@naver.com>
2023-11-09 11:04:31 +0900

    [FEAT] UIViewController+Rx 코드 추가 (#399)

--- Maddori.Apple/Maddori.Apple/Global/Extension/Rx/UIViewController+Rx.swift
+++ Maddori.Apple/Maddori.Apple/Global/Extension/Rx/UIViewController+Rx.swift
@@ -5,4 +5,29 @@
 //  Created by 이성민 on 11/9/23.
 //
 
-import Foundation
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+public extension Reactive where Base: UIViewController {
+    var viewDidLoad: Observable<Void> {
+        return self.methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
+    }
+    
+    var viewWillAppear: Observable<Void> {
+        return self.methodInvoked(#selector(Base.viewWillAppear)).map { _ in }
+    }
+    
+    var viewDidAppear: Observable<Void> {
+        return self.methodInvoked(#selector(Base.viewDidAppear)).map { _ in }
+    }
+    
+    var viewWillDisappear: Observable<Void> {
+        return self.methodInvoked(#selector(Base.viewWillDisappear)).map { _ in }
+    }
+    
+    var viewDidDisappear: Observable<Void> {
+        return self.methodInvoked(#selector(Base.viewDidDisappear)).map { _ in }
+    }
+}
