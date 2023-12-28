575173c0cd828753a171bba34154e29ed1199c77
seongmin221 <seongmin221@naver.com>
2023-12-29 01:07:53 +0900

    [ADD] UserInfo added (#403)

---
+++ Maddori.Apple/Maddori.Apple/New Group/Entity/Feedback/TeamFeedback.swift
@@ -0,0 +1,8 @@
+//
+//  TeamFeedback.swift
+//  Maddori.Apple
+//
+//  Created by 이성민 on 12/29/23.
+//
+
+import Foundation
---
+++ Maddori.Apple/Maddori.Apple/New Group/Entity/UserInfo.swift
@@ -0,0 +1,8 @@
+//
+//  UserInfo.swift
+//  Maddori.Apple
+//
+//  Created by 이성민 on 12/29/23.
+//
+
+import Foundation
