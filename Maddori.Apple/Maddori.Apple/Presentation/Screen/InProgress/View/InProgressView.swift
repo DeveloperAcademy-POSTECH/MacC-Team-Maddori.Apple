868f4684b8f82bc3abc418ddf7ec19688b699e67
seongmin221 <seongmin221@naver.com>
2023-12-30 17:21:26 +0900

    [ADD] new inprogress view, vc added (#403)

---
+++ Maddori.Apple/Maddori.Apple/Presentation/Screen/InProgress/View/InProgressView.swift
@@ -0,0 +1,8 @@
+//
+//  InProgressView.swift
+//  Maddori.Apple
+//
+//  Created by 이성민 on 12/30/23.
+//
+
+import Foundation
---
+++ Maddori.Apple/Maddori.Apple/Presentation/Screen/InProgress/ViewController/NewInProgressViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NewInProgressViewController.swift
+//  Maddori.Apple
+//
+//  Created by 이성민 on 12/30/23.
+//
+
+import Foundation
