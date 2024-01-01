32a1fef7ff273d96ca35dbee1aa1f95a4b7834f7
seongmin221 <seongmin221@naver.com>
2024-01-01 15:30:42 +0900

    [CHORE] FeedbackInfo dummy added

--- Maddori.Apple/Maddori.Apple/New Group/Entity/Feedback/FeedbackInfo.swift
+++ Maddori.Apple/Maddori.Apple/New Group/Entity/Feedback/FeedbackInfo.swift
@@ -25,3 +25,15 @@ extension FeedbackInfo: Hashable {
         lhs.id == rhs.id
     }
 }
+
+extension FeedbackInfo {
+    static func dummy() -> [FeedbackInfo] {
+        return [
+            .init(id: 0, type: .continueType, keyword: "testawvaweio1", content: "test2", fromUser: .init(id: 9, nickname: "test3")),
+            .init(id: 1, type: .continueType, keyword: "2", content: "test2", fromUser: .init(id: 9, nickname: "test3")),
+            .init(id: 2, type: .continueType, keyword: "afe3", content: "test2", fromUser: .init(id: 9, nickname: "test3")),
+            .init(id: 3, type: .continueType, keyword: "afr4", content: "test2", fromUser: .init(id: 9, nickname: "test3")),
+            .init(id: 4, type: .continueType, keyword: "a5", content: "test2", fromUser: .init(id: 9, nickname: "test3")),
+        ]
+    }
+}
