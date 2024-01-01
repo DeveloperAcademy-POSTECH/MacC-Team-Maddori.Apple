7dbdbf9ca894ea87385d66499410cc2e94694c4a
seongmin221 <seongmin221@naver.com>
2024-01-01 15:35:45 +0900

    [CHORE] keyword datasource edited (#403)

--- Maddori.Apple/Maddori.Apple/Presentation/Screen/InProgress/View/KeywordDataSource.swift
+++ Maddori.Apple/Maddori.Apple/Presentation/Screen/InProgress/View/KeywordDataSource.swift
@@ -5,4 +5,92 @@
 //  Created by 이성민 on 12/31/23.
 //
 
-import Foundation
+import UIKit
+
+final class KeywordDataSource<Section: Hashable & Sendable & CaseIterable & RawRepresentable> {
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, FeedbackInfo>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, FeedbackInfo>
+    
+    typealias Cell = NewKeywordCollectionViewCell
+    typealias CellRegistration = UICollectionView.CellRegistration<Cell, FeedbackInfo>
+    
+    typealias Header = KeywordCollectionViewHeader
+    typealias HeaderRegistration = UICollectionView.SupplementaryRegistration<Header>
+    
+    // MARK: - property
+    
+    let collectionView: UICollectionView
+    let hasHeader: Bool
+    
+    var dataSource: DataSource!
+    var snapshot: Snapshot!
+    
+    // MARK: - init
+    
+    init(collectionView: UICollectionView, hasHeader: Bool) {
+        self.collectionView = collectionView
+        self.hasHeader = hasHeader
+        self.setupDataSource()
+        self.setupSnapshot()
+    }
+    
+    // MARK: - setup
+    
+    private func setupDataSource() {
+        self.configureCell()
+        if hasHeader { self.configureHeader() }
+    }
+    
+    private func setupSnapshot() {
+        self.snapshot = Snapshot()
+        
+        let sections = Array(Section.allCases)
+        self.snapshot.appendSections(sections)
+        self.dataSource.apply(self.snapshot)
+    }
+}
+
+// MARK: - configure dataSource
+
+extension KeywordDataSource {
+    private func configureCell() {
+        let cellRegistration = CellRegistration { cell, _, feedback in
+            cell.configureLabel(text: feedback.keyword)
+            cell.configureUI(type: .main)
+        }
+        
+        self.dataSource = DataSource(collectionView: self.collectionView, cellProvider: { collectionView, indexPath, feedback in
+            return self.collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: feedback
+            )
+        })
+    }
+    
+    private func configureHeader() {
+        let headerRegistration = HeaderRegistration(
+            elementKind: UICollectionView.elementKindSectionHeader
+        ) { header, _, indexPath in
+            let titles = Section.allCases.map { String(describing: $0.rawValue) }
+            header.configureLabel(text: titles[indexPath.section] )
+        }
+        
+        self.dataSource.supplementaryViewProvider = { _, _, indexPath in
+            return self.collectionView.dequeueConfiguredReusableSupplementary(
+                using: headerRegistration,
+                for: indexPath
+            )
+        }
+    }
+}
+
+// MARK: - configure snapshot
+
+extension KeywordDataSource {
+    func loadSnapshot(with feedbacks: [FeedbackInfo]) {
+        self.snapshot.appendItems(feedbacks)
+        self.dataSource.apply(self.snapshot)
+    }
+}
