e06fb4d34e6f329066e0b243aa4c593872f75758
seongmin221 <seongmin221@naver.com>
2024-01-01 15:31:30 +0900

    [CHORE] KeywordCollectionViewHeader applied (#403)

--- Maddori.Apple/Maddori.Apple/Presentation/Screen/InProgress/View/KeywordCollectionViewHeader.swift
+++ Maddori.Apple/Maddori.Apple/Presentation/Screen/InProgress/View/KeywordCollectionViewHeader.swift
@@ -5,4 +5,39 @@
 //  Created by 이성민 on 12/31/23.
 //
 
-import Foundation
+import UIKit
+
+final class KeywordCollectionViewHeader: UICollectionReusableView {
+    
+    // MARK: - property
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.font = .label2
+        label.textColor = .black100
+        return label
+    }()
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    // MARK: - setup
+    
+    private func setupLayout() {
+        self.addSubview(label)
+        label.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension KeywordCollectionViewHeader {
+    func configureLabel(text: String) {
+        self.label.text = text
+    }
+}
